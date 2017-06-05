1291941c8f006bc6606f6f0e99d7735263492cff
marijagacina <gacinamarija@gmail.com>
2017-06-05 14:54:40 +0200

    Update mapGUI.py

--- mapGUI.py
+++ mapGUI.py
@@ -1,10 +1,7 @@
 import sys
-
 from PyQt4.QtWebKit import *
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-
-
 from qgmap import *
 import pyqtgraph as pg
 import time
@@ -13,12 +10,11 @@ import threading
 import publish_json
 from socket import *
 
-
-"""Global variables that are accessed by multiple classes and functions"""
+"""Global variables"""
 mussels = {}
 threadLock = threading.Condition()
 threads = []
-selectedId =""
+selectedId = ""
 map = 0
 
 """Class used for printing warning messages from Google Maps API"""
@@ -26,35 +22,29 @@ class _LoggedPage(QWebPage):
     def javaScriptConsoleMessage(self, msg, line, source):
         print('JS: %s line %d: %s' % (source, line, msg))
 
-
 """Main widget that is opened when the app is run"""
 class Window(QWidget):
-
     """Initializes the main window by setting its size, icon, title and UI """
     def __init__(self):
         super(QWidget, Window).__init__(self)
         self.setWindowTitle('GUI')
         self.setWindowIcon(QIcon('labust.png'))
         self.setGeometry(50, 50, 1400, 900)
-        self.initUI()
+        self.init_ui()
 
     """Initializes UI by finding mussels, adding map and information widget where 
     mussel  presented"""
-    def initUI(self):
+    def init_ui(self):
         self.layout = QHBoxLayout()
         global map
         w = QDialog()
         map = QGoogleMap(w)
         self.info = InfoWidget()
-
         self.init_map()
-
         self.layout.addWidget(w)
         self.layout.addWidget(self.info)
-
         self.setLayout(self.layout)
         self.show()
-
         publish_thread = PublishThread(2, "Thread 2")
         threads.append(publish_thread)
         publish_thread.start()
@@ -64,6 +54,7 @@ class Window(QWidget):
 
     """Initializes Google Map, sets its size, center and actions when the marker is clicked on"""
     def init_map(self):
+        global map
         map.setFixedSize(850, 900)
         map.waitUntilReady()
         map.centerAt(46.5, 12.5)
@@ -71,28 +62,34 @@ class Window(QWidget):
         map.markerClicked.connect(onMarkerLClick)
         map.markerClicked.connect(self.info.selected_changed)
 
+    """Saves collected data to a file when the main window is closed."""
+    def closeEvent(self, *args, **kwargs):
+        with open("collected_data.txt", "w") as f:
+            for x in list(mussels.keys()):
+                mussel = mussels[x]
+                f.write("Mussel " + mussel.id + "\n")
+                for i in range(len(mussel.time_array)):
+                    f.write("time %02d:%02d:%02d" % ((mussel.time_array[i] % 86400) // 3600 + 2, (mussel.time_array[i] % 3600) // 60, mussel.time_array[i] % 60))
+                    f.write(" lat " + str(mussel.latitude_array[i]) + " lng " + str(mussel.longitude_array[i]) + " battery " + str(mussel.battery_array[i]) + "\n")
+
 """Function that sets variable selected id to the key of the clicked marker"""
 def onMarkerLClick(key):
     global selectedId
     selectedId = key
-
 
 """Widget that contains information about the selected mussel, showing its properties, and can 
 plot its values."""
 class InfoWidget(QWidget):
     def __init__(self):
         super(QWidget, InfoWidget).__init__(self)
-
         self.setMaximumSize(500, 900)
         self.hide()
         self.layout = QGridLayout()
         self.setLayout(self.layout)
-
         self.display_info()
         self._update_timer = QTimer()
         self._update_timer.timeout.connect(self.update_label_and_map)
         self._update_timer.start(500)  # milliseconds
-
 
     """Funtion that updates the label every 0.5 s and shows """
     def update_label_and_map(self):
@@ -101,7 +98,6 @@ class InfoWidget(QWidget):
             if time.time() - mussel.time_last_signal > 5 * 60:
                 mussel.active = 0
             map.addMarker(mussel.id, mussel.lat, mussel.lng)
-
         if (selectedId == "") or (not mussels.get(selectedId)):
             self.hide()
             return
@@ -112,7 +108,6 @@ class InfoWidget(QWidget):
         self.lat.setText("latitude: " + str(self.selected_mussel.lat))
         self.long.setText("longitude: " + str(self.selected_mussel.lng))
         self.show()
-
 
     """Shows QTabWidget where the information about selected mussel is shown"""
     def display_info(self):
@@ -143,42 +138,34 @@ class InfoWidget(QWidget):
         self.group.addButton(self.long)
 
         self.button_group.setLayout(group_layout)
-        tab1_layout.addWidget(self.button_group)
 
+        tab1_layout.addWidget(self.button_group)
         tab1.setLayout(tab1_layout)
 
-        self.tab.addTab(tab1,'d')
+        self.tab.addTab(tab1,"General")
         self.layout.addWidget(self.tab)
+        pg.setConfigOption("background", "w")
+        pg.setConfigOption("foreground", "k")
 
-        pg.setConfigOption('background', 'w')
-        pg.setConfigOption('foreground', 'k')
-
-        axis = TimeAxisItem(orientation='bottom')
-        axis2 = pg.AxisItem(orientation='left')
-        self.plot = pg.PlotWidget(axisItems={'bottom': axis, 'left': axis2})
+        axis = TimeAxisItem(orientation="bottom")
+        axis2 = pg.AxisItem(orientation="left")
+        self.plot = pg.PlotWidget(axisItems={"bottom": axis, "left": axis2})
         self.plot.layout = QGridLayout()
         self.plot.plotItem.hide()
         self.layout.addWidget(self.plot)
-
-        self.battery_voltage.toggled.connect(lambda: self.plotting_widget(self.selected_mussel.time_array,
-                                                                     self.selected_mussel.battery_array, "Battery voltage"))
-        self.lat.toggled.connect(lambda: self.plotting_widget(self.selected_mussel.time_array,
-                                                         self.selected_mussel.latitude_array, "Latitude"))
-
-        self.long.toggled.connect(lambda: self.plotting_widget(self.selected_mussel.time_array,
-                                                               self.selected_mussel.longitude_array, "Longitude"))
-
+        # action events
+        self.battery_voltage.toggled.connect(lambda: self.plotting_widget(self.selected_mussel.time_array, self.selected_mussel.battery_array, "Battery voltage"))
+        self.lat.toggled.connect(lambda: self.plotting_widget(self.selected_mussel.time_array, self.selected_mussel.latitude_array, "Latitude"))
+        self.long.toggled.connect(lambda: self.plotting_widget(self.selected_mussel.time_array, self.selected_mussel.longitude_array, "Longitude"))
 
     """Plots the real-time values sent by the selected mussel"""
     def plotting_widget(self, time, values, title):
         def update():
-
             self.plot.plotItem.plot(time, values, clear=True)
             self.plot.plotItem.setTitle(title)
             self.plot.plotItem.setMaximumSize(480, 500)
             self.plot.plotItem.show()
             QCoreApplication.processEvents()
-
         self.plot.plotItem.clear()
         self.plot.plotItem.enableAutoScale()
         self.update = update
@@ -206,32 +193,7 @@ class TimeAxisItem(pg.AxisItem):
         super().__init__(*args, **kwargs)
 
     def tickStrings(self, values, scale, spacing):
-        return ['%d:%d:%d' % ((value % 86400) // 3600 + 2, (value % 3600) // 60, value % 60) for value in values]
-
-
-
-
-"""Class thar represents a mussel and defines the properties that it contains"""
-class Mussel():
-    def __init__(self, id, lat, lng, batteryVoltage, chargingStatus):
-        self.id = id
-        self.latitude_array = []
-        self.longitude_array = []
-        self.battery_array = []
-        self.time_array = []
-        self.update(lat, lng, batteryVoltage, chargingStatus)
-    def update(self, lat, lng, battery_voltage, charging_status):
-        self.lat = lat
-        self.lng = lng
-        self.longitude_array.append(lng)
-        self.latitude_array.append(lat)
-        self.battery_array.append(battery_voltage)
-        self.battery_voltage = battery_voltage
-        self.charging_status = charging_status
-        self.time_last_signal = time.time()
-        self.time_array.append(time.time())
-        self.active = 1
-        self.updated = True
+        return ['%02d:%02d:%02d' % ((value % 86400) // 3600 + 2, (value % 3600) // 60, value % 60) for value in values]
 
 """Thread that finds the messages sent by the mussels via Wi-Fi connection"""
 class FindThread(QThread):
@@ -248,34 +210,28 @@ class FindThread(QThread):
         global mussels
         while True:
             threadLock.acquire()
-
             s = socket(AF_INET, SOCK_DGRAM)
             s.bind(('localhost', 4545))
             threadLock.wait()
-
             n = s.recv(2048)
             m = n.decode('utf-8')
             stat_obj = json.loads(m)
-
             if not m:
                 continue
             threadLock.acquire()
-            for i in range(0, len(stat_obj['mussels'])):
-                muss = stat_obj['mussels'][i]
-                self.file = open('collected_data.txt', 'r+')
-                if muss['ID'] not in list(mussels.keys()):
-                    mussels[muss['ID']] = Mussel(muss['ID'], muss['lat'], muss['lng'], muss['battery'],
-                                                 muss['charging'])
-                    print (mussels[muss['ID']].id+'\n')
-                    self.file.write(mussels[muss['ID']].id)
+            for i in range(0, len(stat_obj["mussels"])):
+                muss = stat_obj["mussels"][i]
+                if muss["ID"] not in list(mussels.keys()):
+                    mussels[muss["ID"]] = Mussel(muss["ID"], muss["lat"], muss["lng"], muss["battery"],
+                                                 muss["charging"])
+                    print (mussels[muss["ID"]].id + "\n")
                 else:
-                    mussels[muss['ID']].update(muss['lat'], muss['lng'], muss['battery'], muss['charging'])
+                    mussels[muss["ID"]].update(muss["lat"], muss["lng"], muss["battery"], muss["charging"])
             threadLock.release()
+
     def end(self):
         self.file.close()
         self._stop()
-
-
 
 """Thread that runs publish_json.py that publishes mussel information to Wi-Fi connection"""
 class PublishThread(QThread):
@@ -296,12 +252,31 @@ class PublishThread(QThread):
     def end(self):
         self._stop()
 
+"""Class thar represents a mussel and defines the properties that it contains"""
+class Mussel():
+    def __init__(self, id, lat, lng, batteryVoltage, chargingStatus):
+        self.id = id
+        self.latitude_array = []
+        self.longitude_array = []
+        self.battery_array = []
+        self.time_array = []
+        self.update(lat, lng, batteryVoltage, chargingStatus)
+
+    def update(self, lat, lng, battery_voltage, charging_status):
+        self.lat = lat
+        self.lng = lng
+        self.longitude_array.append(lng)
+        self.latitude_array.append(lat)
+        self.battery_array.append(battery_voltage)
+        self.battery_voltage = battery_voltage
+        self.charging_status = charging_status
+        self.time_last_signal = time.time()
+        self.time_array.append(time.time())
+        self.active = 1
+        self.updated = True
 
 """Main program that starts when the app is run. Initializes the main window"""
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
     sys.exit(app.exec_())
-    for thr in threads:
-        thr.end()
